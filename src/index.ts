6508bb36306093932452ad46a3cbe5371a020163
Chris Reams <chrisreams91@gmail.com>
2022-07-06 17:19:05 -0500

    initial

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Client, Intents } from "discord.js";
+import { token } from "../config.json";
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+// client.login(token);
+
+console.log("bccm");
