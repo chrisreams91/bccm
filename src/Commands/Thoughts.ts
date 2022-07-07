8c3c60f34b361e0619b3b10e08dbe96e4e6ce2c3
BenP <bdprice0527@gmail.com>
2022-07-07 10:21:10 -0500

    thughots?

---
+++ src/Commands/Thoughts.ts
@@ -0,0 +1,33 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+
+export const thoughtsCommandName = "thoughts";
+
+export const thoughtsCommand = new SlashCommandBuilder()
+  .setName(thoughtsCommandName)
+  .setDescription("thohguts?");
+
+export const thoughtsCommandHandler = async (
+  interaction: CommandInteraction
+) => {
+  const str = scramble("thoughts");
+  
+  await interaction.reply(str);
+};
+
+const scramble = (string: String) => {
+  let scrambled = string.split("");
+  for (let i = 1; i < string.length; i++) {
+    for (let j = i-1; j < i; j--) {
+      if (Math.random() >= 0.75) {
+        const tmp = scrambled[i]
+        scrambled[i] = scrambled[j];
+        scrambled[j] = tmp;
+      }
+      else {
+        break;
+      }
+    }
+  }
+  return scrambled.join("") + "?";
+}
